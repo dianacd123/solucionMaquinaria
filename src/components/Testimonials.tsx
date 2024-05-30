a04152eb16f3ab0b069894513fc94a7f9fdd9a2d
alexis <asalvadormartinezc@gmail.com>
2024-05-29 18:15:24 -0600

    cambios A

---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,94 @@
+import { motion } from 'framer-motion';
+import imagenNatalia from '../assets/natalia.jpg';
+import imagenJulia from '../assets/julia.jpg';
+import imagenPedro from '../assets/pedro.jpg';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const Test = () => {
+  const testimonios = [
+    {
+      nombre: "Natalia",
+      testimonio: "Excelente servicio y maquinaria de calidad: Estoy muy impresionado con la atención al cliente y la calidad de la maquinaria proporcionada por Tu Solución en Maquinaria. Su equipo experto me ayudó a seleccionar la maquinaria adecuada para mi proyecto, y la entrega fue puntual y sin problemas.",
+      imagen: imagenNatalia
+    },
+    {
+      nombre: "Julia",
+      testimonio: "Desde el primer contacto hasta la recogida de la maquinaria al finalizar mi proyecto, La cotización fue clara y detallada, la entrega de la maquinaria fue rápida y el soporte técnico durante el alquiler fue excepcional. ¡Sin duda volveré a trabajar con ellos en el futuro!",
+      imagen: imagenJulia
+    },
+    {
+      nombre: "Pedro",
+      testimonio: "Alquilar maquinaria con Tu Solución en Maquinaria fue una experiencia sin complicaciones de principio a fin. Su equipo fue amable, atento y siempre disponible para responder a mis preguntas y brindar asesoramiento experto. ¡Gracias por hacer que mi proyecto sea un éxito!",
+      imagen: imagenPedro
+    }
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 5000,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 500,
+    cssEase: 'ease-out',
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
+  };
+
+  return (
+    <div className="bg-black font-poppins min-h-2">
+      <div className="text-center py-10">
+        <h2 className="text-6xl font-bold text-amber-300">Usuarios de "Tu solución en Maquinarias" satisfechos</h2>
+      </div>
+
+      <motion.div
+        variants={{ hidden: { opacity: 0 }, show: { opacity: 1 } }}
+        initial="hidden"
+        animate="show"
+        viewport={{ once: false, amount: 0.7 }}
+      >
+        <section className=" bg-black text-white">
+          <div className="w-full ">
+            <Slider {...settings}>
+              {testimonios.map((testimonio, index) => (
+                <div key={index} className="px-2">
+                  <div className="bg-black p-6 rounded-lg text-center h-full flex flex-col justify-between">
+                    <div className="bg-[#fde502] flex-1 flex flex-col justify-center py-7 rounded-sm" >
+                      <img src={testimonio.imagen} alt={testimonio.nombre} className="w-20 h-20 rounded-full mx-auto mb-4" />
+                      <h3 className="text-xl font-bold text-black mb-2">{testimonio.nombre}</h3>
+                    </div>
+                    <div className="bg-white p-6 rounded-b-lg">
+                      <p className="text-md font-medium text-black md:text-lg">
+                        {testimonio.testimonio}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </section>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Test;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@
 import BannerP from "../components/BannerP";
 import { Component } from "../components/Footer";
 import Header from "../components/Header";
+import Test from "../components/Testimonials";
 
 export default function Home() {
   return (
     <div>
         <Header></Header>
         <BannerP></BannerP>
+        <Test></Test>
         <Component></Component>
     </div>
   )
