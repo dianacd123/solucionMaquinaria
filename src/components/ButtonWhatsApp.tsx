1316235558b58872e2e1cc9512634d8b68b62ce2
dianacd123 <dianacd1234567@gmail.com>
2024-06-03 23:05:49 -0600

    commit

--- src/components/ButtonWhatsApp.tsx
+++ src/components/ButtonWhatsApp.tsx
@@ -2,8 +2,8 @@ import { Button } from 'flowbite-react';
 import { BsWhatsapp } from "react-icons/bs";
 
 export default function ButtonWhatsApp() {
-    const phoneNumber = '+52 55 87674615'; // Replace with your actual WhatsApp number
-    const message = 'Hola! Me gustaría obtener más información.'; // Replace with your desired message
+    const phoneNumber = '+52 55 87674615'; 
+    const message = 'Hola! Me gustaría obtener más información.'; 
   
     const WhatsAppLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
   
