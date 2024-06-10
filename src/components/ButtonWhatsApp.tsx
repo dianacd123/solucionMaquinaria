8fcff227959ad61009757769bfad0d556212e2fd
dianacd123 <dianacd1234567@gmail.com>
2024-06-10 16:19:36 -0600

    commit

--- src/components/ButtonWhatsApp.tsx
+++ src/components/ButtonWhatsApp.tsx
@@ -8,10 +8,10 @@ export default function ButtonWhatsApp() {
     const WhatsAppLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
   
     return (
-      <Button  size="lg" target='blank'
+      <Button  size="lg" target='blank' 
         className='fixed bottom-0 right-0 bg-[#25D366]
          text-white font-bold p-2 m-8 rounded-full
-          z-50 hover:bg-[#209e4e]' 
+          z-50 hover:bg-[#209e4e] ' 
         href={WhatsAppLink} color="success">
         <BsWhatsapp className='h-10 w-10'></BsWhatsapp>
       </Button>
