2bc4dcf4a3af0f767778623fdc7c50b74db5b60b
dianacd123 <dianacd1234567@gmail.com>
2024-06-10 12:58:01 -0600

    icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,36 +20,36 @@ export function Component() {
         <div>
           <FooterTitle className="text-x1 font-bold text-white" title="EXPLORA" />
           <FooterLinkGroup col className="text-white">
-            <FooterLink className="text-gray-400" href="#">¿Quienes somos?</FooterLink>
-            <FooterLink className="text-gray-400" href="#">Servicios</FooterLink>
-            <FooterLink className="text-gray-400" href="#">Maquinaria</FooterLink>
-            <FooterLink className="text-gray-400" href="#">Testimonios</FooterLink>
+            <FooterLink className="text-gray-300" href="#">¿Quienes somos?</FooterLink>
+            <FooterLink className="text-gray-300" href="#">Servicios</FooterLink>
+            <FooterLink className="text-gray-300" href="#">Maquinaria</FooterLink>
+            <FooterLink className="text-gray-300" href="#">Testimonios</FooterLink>
           </FooterLinkGroup>
         </div>
         <div>
         <div>
           <FooterTitle className="text-x1 font-bold text-white" title="LEGAL" />
           <FooterLinkGroup col className="text-white">
-            <FooterLink className="text-gray-400" href="#">Políticas de Privacidad</FooterLink>
-            <FooterLink className="text-gray-400" href="#">Términos &amp; Condiciones</FooterLink>
+            <FooterLink className="text-gray-300" href="#">Políticas de Privacidad</FooterLink>
+            <FooterLink className="text-gray-300" href="#">Términos &amp; Condiciones</FooterLink>
           </FooterLinkGroup>
         </div>
         <br></br>
           <FooterTitle className="text-x1 font-bold text-white" title="SIGUENOS" />
-          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
-            <FooterIcon className="text-gray-400" href="#" icon={BsFacebook} />
-            <FooterIcon className="text-gray-400" href="#" icon={BsInstagram} />
-            <FooterIcon className="text-gray-400" href="#" icon={BsTwitter} />
-            <FooterIcon className="text-gray-400" href="#" icon={BsTiktok} />
+          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-left">
+            <FooterIcon className="text-gray-300" href="#" icon={BsFacebook} />
+            <FooterIcon className="text-gray-300" href="#" icon={BsInstagram} />
+            <FooterIcon className="text-gray-300" href="#" icon={BsTwitter} />
+            <FooterIcon className="text-gray-300" href="#" icon={BsTiktok} />
           </div>
         </div>
         <div>
           <FooterTitle className="text-x1 font-bold text-white" title="Servicio al cliente" />
           <FooterLinkGroup col className="text-white">
           <div className="text-white">
-            <p className="text-sm text-gray-400">Teléfono: (999) 123 4567</p>
-            <p className="text-sm text-gray-400">Dirección: Calle 56, Mérida, 97000</p>
-            <p className="text-sm text-gray-400">Email: hola@sitioincreible.mx</p>
+            <p className="text-sm text-gray-300">Teléfono: (999) 123 4567</p>
+            <p className="text-sm text-gray-300">Dirección: Calle 56, Mérida, 97000</p>
+            <p className="text-sm text-gray-300">Email: hola@sitioincreible.mx</p>
           </div>
           </FooterLinkGroup>
         </div>
