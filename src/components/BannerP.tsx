b62f83f1f7339e8d0fb2d03f430a9cf34cf6df3c
Belldandy Zarate <144074730+BelldandyZarate@users.noreply.github.com>
2024-06-12 13:40:07 -0600

    Update BannerP.tsx

--- src/components/BannerP.tsx
+++ src/components/BannerP.tsx
@@ -8,10 +8,13 @@ export default function BannerP() {
     <div className="  sm:flex md:h-[550px]">
   <div className="flex-1 justify-center  p-10   bg-[#FDE502] flex flex-col">
     <h1 className="text-[#0b0a09] font-bold sm:text-6xl text-5xl">
-      ¿Listo para llevar su proyecto al siguiente nivel?
+      ¿Necesitas maquinaria para su proyecto?
     </h1>
     <h2 className="text-[#0b0a09] text-lg py-8">
-      ¡Contáctenos hoy mismo para conocer más sobre nuestros servicios de alquiler de maquinaria y cómo podemos ayudarle a alcanzar sus objetivos!
+      ¡Contáctenos hoy mismo y obtenga una cotización gratuita!
+    <br></br>
+      Explore nuetros servicios de <strong> venta y alquiler de maquinaria </strong>
+       y descubra cómo podemos ayudarle a alcanzar sus objetivos de manera eficiente y efectiva.
     </h2>
     <div className=" ">
       <ButtonCTA></ButtonCTA>
@@ -25,4 +28,3 @@ export default function BannerP() {
     
   )
 }
-
