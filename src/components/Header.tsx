f3a46e6ab34c18fbe4a2e4dc407cc459def9e21d
dianacd123 <dianacd1234567@gmail.com>
2024-06-21 15:32:24 -0600

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
+import logo from  "../assets/LogoWhite.png"
 
 export default function Header() {
   return (
     <div className="flex flex-row bg-[#0B0A09] p-6 sm:p-6">
   <div className="flex flex-1 pl-5 justify-center sm:justify-start items-center">
-    <img className="h-[55px] sm:h-20 m-0" src="../src/assets/LogoWhite.png" alt="" />
+    <img className="h-[55px] sm:h-20 m-0" src={logo} alt="" />
   </div>
 </div>
   )
