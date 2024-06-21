6b8d1afea80a78d8ecd28786dfca1b9b061ae717
dianacd123 <dianacd1234567@gmail.com>
2024-06-21 14:43:39 -0600

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,10 @@
 
 export default function Header() {
   return (
-    
     <div className="flex flex-row bg-[#0B0A09] p-6 sm:p-6">
   <div className="flex flex-1 pl-5 justify-center sm:justify-start items-center">
     <img className="h-[55px] sm:h-20 m-0" src="../src/assets/LogoWhite.png" alt="" />
   </div>
 </div>
-
-
-
-    
   )
 }
