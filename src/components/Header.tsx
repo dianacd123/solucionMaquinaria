ba901e39391f79fef76324013f37b42e40e18a54
dianacd123 <dianacd1234567@gmail.com>
2024-06-21 14:42:53 -0600

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,14 @@
 export default function Header() {
   return (
     
-    <div className="flex p-6 sm:p-6 flex-row  bg-[#0B0A09]">
-    <div className="flex-1 flex justify-left items-left pl-5">
-      <img className="h-20" src="../src/assets/LogoWhite.png" alt="" />
-      <h1 className="text-white hidden font-bold text">TU SOLUCIÓN EN <br /> MAQUINARIA</h1>
-    </div>
-    <div className="flex-1 w-30 flex justify-left items-left">
-      <h1 className="text-white text-2xl"></h1>
-    </div>
-    <div className="flex-1  flex justify-center items-center">
-      <h1 className="text-white hidden text">LOGOTIPO</h1>
-    </div>
+    <div className="flex flex-row bg-[#0B0A09] p-6 sm:p-6">
+  <div className="flex flex-1 pl-5 justify-center sm:justify-start items-center">
+    <img className="h-[55px] sm:h-20 m-0" src="../src/assets/LogoWhite.png" alt="" />
   </div>
+</div>
+
+
+
     
   )
 }
