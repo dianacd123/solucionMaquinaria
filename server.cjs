486949f4563259f62e25533777ba2a6b811c17b2
alexis <asalvadormartinezc@gmail.com>
2024-06-21 20:17:49 -0600

    c

--- server.cjs
+++ server.cjs
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config({ path: './cred.env' });
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const transporter = nodemailer.createTransport({
   host: 'smtp-mail.outlook.com',
@@ -22,26 +22,25 @@ app.use(cors({ origin: 'https://rentamaquinaria.promarketconnect.com' }));
 app.use(bodyParser.json());
 
 app.post('/send-email', async (req, res) => {
-  const { nombre, email, telefono, proyecto, maquinaria } = req.body;
+  const { nombre, email, telefono, proyecto, maquinaria, tipo } = req.body;
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: 'gsanchez@promarketconnect.com',
     subject: 'Nueva Cotización',
-    text: `Nombre: ${nombre}\nCorreo: ${email}\nTeléfono: ${telefono}\nDetalles del Proyecto: ${proyecto}\nMaquinaria Necesaria: ${maquinaria}`,
+    text: `Nombre: ${nombre}\nCorreo: ${email}\nTeléfono: ${telefono}\nDetalles del Proyecto: ${proyecto}\nMaquinaria Necesaria: ${maquinaria}\nTipo de Solicitud: ${tipo}`,
   };
 
   try {
     const info = await transporter.sendMail(mailOptions);
     console.log('\x1b[32m', 'Correo enviado:', info.messageId);
-    res.status(200).send('Correo enviado con éxito'); 
+    res.status(200).send('Correo enviado con éxito');
   } catch (error) {
     console.error('\x1b[31m', 'Error al enviar el correo:', error);
-    res.status(500).send('Error al enviar el correo'); 
+    res.status(500).send('Error al enviar el correo');
   }
 });
 
 app.listen(PORT, () => {
   console.log('\x1b[36m', `Servidor ejecutándose en el puerto ${PORT}`);
 });
-//hola hola
