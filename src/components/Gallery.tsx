da66ac53d646a98d22224c9c9232b42a606bb4c7
Belldandy Zarate <144074730+BelldandyZarate@users.noreply.github.com>
2024-06-24 18:24:03 -0600

    Update Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -5,39 +5,36 @@ import img2 from '../assets/B320NB.png';
 import img3 from '../assets/B416FNB.png';
 import img4 from '../assets/B320EP.png';
 
-const galleryStyle: React.CSSProperties = {
-  display: 'flex',
-  justifyContent: 'space-around',
-  flexWrap: 'wrap',
-  gap: '20px',
-};
-
 const Gallery: React.FC = () => {
   return (
     <div id="Maquinaria" className="bg-[#0B0A09] p-20">
       <div className="flex justify-center mb-8">
         <h1 className="text-[#FDE502] font-bold text-5xl">MAQUINARIA DISPONIBLE</h1>
       </div>
-      <div style={galleryStyle}>
+      <div className="flex justify-around flex-wrap gap-20">
         <ImageInfo
           src={img1}
           alt="Retroexcavadora caterpillar 420F"
           info="Retroexcavadora caterpillar 420F"
+          link="/productDetail1" // Usar rutas absolutas para navegación
         />
         <ImageInfo
           src={img2}
           alt="Excavadora caterpillar 320"
           info="Excavadora caterpillar 320"
+          link="/productDetail2" // Usar rutas absolutas para navegación
         />
         <ImageInfo
           src={img3}
           alt="Retroexcavadora caterpillar 416F"
           info="Retroexcavadora caterpillar 416F"
+          link="/productDetail3" // Usar rutas absolutas para navegación
         />
         <ImageInfo
           src={img4}
           alt="Excavadora caterpillar 320E"
           info="Excavadora caterpillar 320E"
+          link="/productDetail4" // Usar rutas absolutas para navegación
         />
       </div>
     </div>
@@ -45,3 +42,4 @@ const Gallery: React.FC = () => {
 };
 
 export default Gallery;
+
