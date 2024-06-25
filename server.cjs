b7ef4b662e1d12caf1bccdcdc0b029d84bed3edd
alexis <asalvadormartinezc@gmail.com>
2024-06-25 17:19:38 -0600

    c

--- server.cjs
+++ server.cjs
@@ -6,22 +6,14 @@ require('dotenv').config({ path: './cred.env' });
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['https://rentamaquinaria.promarketconnect.com/', 'http://localhost:5173', 'http://localhost:3000'], // Actualiza esto con tu dominio real
   optionsSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 console.log('Email User:', process.env.EMAIL_USER);
@@ -45,9 +37,6 @@ transporter.verify((error, success) => {
   }
 });
 
-app.use(cors({ origin: 'http://localhost:3000' }));
-app.use(bodyParser.json());
-
 app.post('/send-email', async (req, res) => {
   const { nombre, email, telefono, proyecto, maquinaria, tipo } = req.body;
 
@@ -58,9 +47,7 @@ app.post('/send-email', async (req, res) => {
     text: `Nombre: ${nombre}\nCorreo: ${email}\nTeléfono: ${telefono}\nDetalles del Proyecto: ${proyecto}\nMaquinaria Necesaria: ${maquinaria}\nTipo de Solicitud: ${tipo}`,
   };
 
- 
   try {
-   
     const info = await transporter.sendMail(mailOptions);
     console.log('Correo enviado:', info.messageId);
     res.status(200).send('Correo enviado con éxito');
@@ -69,6 +56,7 @@ app.post('/send-email', async (req, res) => {
     res.status(500).send('Error al enviar el correo');
   }
 });
+
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
