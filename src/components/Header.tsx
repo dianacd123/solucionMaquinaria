58bba69802c7dbad70a6a4e0691f842ac051e49d
dianacd123 <dianacd1234567@gmail.com>
2024-07-01 19:23:16 -0600

    link logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import logo from  "../assets/LogoWhite.png"
+import { Link } from "react-router-dom"
 
 export default function Header() {
   return (
     <div className="flex flex-row bg-[#0B0A09] p-6 sm:p-6">
   <div  className="flex flex-1 pl-5 justify-left sm:justify-start items-left">
-   <a href="">
+    <Link to="/">
     <img className="h-[55px] sm:h-20 m-0" src={logo} alt="" />
-    </a>
+    </Link>
   </div>
 </div>
   )
