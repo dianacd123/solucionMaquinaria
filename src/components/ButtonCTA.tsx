4fd2ac8fdb18cead09ad3669356ecac83093ff97
alexis <asalvadormartinezc@gmail.com>
2024-07-01 19:11:57 -0600

    c

--- src/components/ButtonCTA.tsx
+++ src/components/ButtonCTA.tsx
@@ -20,7 +20,7 @@ export function ButtonCTA() {
     };
 
     try {
-      const response = await fetch('http://localhost:3000/send-email', {
+      const response = await fetch('https://rentamaquinaria.promarketconnect.com/send-email', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
