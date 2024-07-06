a148d81a4e77b194b2038b7135673b968105652f
alexis <asalvadormartinezc@gmail.com>
2024-07-05 21:57:52 -0600

    C

--- src/components/ServicesP.tsx
+++ src/components/ServicesP.tsx
@@ -20,22 +20,22 @@ const ServicesP: React.FC = () => {
             className="text-center text-4xl font-bold text-black md:text-5xl"
             style={estiloH1}
           >
-            Nuestros Servicios
+            NUESTROS SERVICIOS
           </h2>
           <br />
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 font-bold">
             <Card
-              title="Alquiler de Maquinaria"
+              title="ALQUILER DE MAQUINARIA"
               imageSrc={imagen2}
               description1="Ponemos a disposición de nuestros clientes una extensa flota de maquinaria, incluyendo excavadoras, grúas, montacargas, tractores, y más, para cumplir con sus requerimientos específicos de construcción y operaciones industriales."
             />
             <Card
-              title="Asesoramiento Profesional"
+              title="ASESORAMIENTO PROFESIONAL"
               imageSrc={imagen3}
               description1="Nuestro equipo de expertos está disponible para asesorar a nuestros clientes en la selección de la maquinaria más adecuada para sus proyectos, teniendo en cuenta sus necesidades técnicas, presupuesto y plazos de entrega."
             />
             <Card
-              title="Mantenimiento y Soporte Técnico"
+              title="MANTENIMIENTO Y SOPORTE TÉCNICO"
               imageSrc={imagen4}
               description1="Mantenemos nuestras máquinas en óptimas condiciones a través de un riguroso programa de mantenimiento preventivo, y nuestro equipo de técnicos especializados está disponible para brindar soporte técnico en caso de cualquier eventualidad durante el período de alquiler."
             />
