9f0f563c32237b700842cfad37a9d2e69d5fc665
dianacd123 <dianacd1234567@gmail.com>
2024-07-05 22:26:36 -0600

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="flex flex-row bg-[#0B0A09] p-6 sm:p-6">
+    <div className="flex flex-row bg-[#0B0A09] p-6 sm:p-5">
       <div className="justify-left items-left flex flex-1 pl-5 sm:justify-start">
         <Link to="/">
           <img className="m-0 h-[55px] sm:h-20" src={logo} alt="" />
