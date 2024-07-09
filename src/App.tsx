152b8dc60ae477f32f50266608f36761894dac77
Belldandy Zarate <144074730+BelldandyZarate@users.noreply.github.com>
2024-07-09 14:44:33 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductDetail1 from "./pages/ProductDetail1";
 import ProductDetail2 from "./pages/ProductDetail2";
 import ProductDetail3 from "./pages/ProductDetail3";
 import ProductDetail4 from "./pages/ProductDetail4";
 import ScrollToTop from "./components/ScrollToTop";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <main className="h-screen bg-[#0B0A09]">
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/ProductDetail1" element={<ProductDetail1 />} />
-          <Route path="/ProductDetail2" element={<ProductDetail2 />} />
-          <Route path="/ProductDetail3" element={<ProductDetail3 />} />
-          <Route path="/ProductDetail4" element={<ProductDetail4 />} />
-        </Routes>
-      </BrowserRouter>
-    </main>
+    <Router>
+      <ScrollToTop /> {/* Asegúrate de agregar esto */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ProductDetail1" element={<ProductDetail1 />} />
+        <Route path="/ProductDetail2" element={<ProductDetail2 />} />
+        <Route path="/ProductDetail3" element={<ProductDetail3 />} />
+        <Route path="/ProductDetail4" element={<ProductDetail4 />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
