4933ca47c35aa7c02ea375db790d6fd4e462fe0e
Belldandy Zarate <144074730+BelldandyZarate@users.noreply.github.com>
2024-07-15 12:36:28 -0600

    Update DetailM.tsx

--- src/components/DetailM.tsx
+++ src/components/DetailM.tsx
@@ -13,7 +13,6 @@ import image3 from "../assets/image3.jpeg";
 import image4 from "../assets/image4.jpeg";
 import image5 from "../assets/image5.jpeg";
 
-// Props para el modal de imagen expandida
 interface ImageModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -22,7 +21,6 @@ interface ImageModalProps {
   setSelectedImage: React.Dispatch<React.SetStateAction<string>>;
 }
 
-// Componente del modal de imagen expandida
 const ImageModal: React.FC<ImageModalProps> = ({
   isOpen,
   onClose,
@@ -45,34 +43,39 @@ const ImageModal: React.FC<ImageModalProps> = ({
   };
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
-      <div className="relative max-w-full max-h-full">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75 p-4">
+      <div className="relative max-w-full max-h-full bg-white rounded-lg p-7">
         <button
-          className="absolute top-4 right-4 text-white text-4xl z-50 focus:outline-none"
+          className="absolute top-1 right-1 text-black text-4xl z-50 focus:outline-none"
           onClick={onClose}
-          style={{ color: "#ff0000" }} 
+          style={{ color: "#0F4BAD" }}
         >
           &times;
         </button>
-        <img
-          src={selectedImage}
-          alt="Selected"
-          className="max-w-full max-h-full"
-        />
-        <button
-          className="absolute top-1/2 transform -translate-y-1/2 left-4 text-white text-4xl z-50 focus:outline-none"
-          onClick={handlePrevImage}
-          style={{ color: "#ff0000" }}
-        >
-          &#8249;
-        </button>
-        <button
-          className="absolute top-1/2 transform -translate-y-1/2 right-4 text-white text-4xl z-50 focus:outline-none"
-          onClick={handleNextImage}
-          style={{ color: "#ff0000" }}
-        >
-          &#8250;
-        </button>
+        <div className="relative flex flex-col items-center">
+          <img
+            src={selectedImage}
+            alt="Selected"
+            className="max-w-full max-h-full object-contain mb-4"
+            style={{ maxHeight: '70vh', maxWidth: '70vw' }}
+          />
+          <div className="flex justify-between w-full">
+            <button
+              className="text-black text-4xl z-50 focus:outline-none"
+              onClick={handlePrevImage}
+              style={{ color: "#0F4BAD" }}
+            >
+              &#8249;
+            </button>
+            <button
+              className="text-black text-4xl z-50 focus:outline-none"
+              onClick={handleNextImage}
+              style={{ color: "#0F4BAD" }}
+            >
+              &#8250;
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -143,26 +146,11 @@ const DetailM: React.FC = () => {
                   <span className="ml-auto text-gray-900">4</span>
                 </div>
                 <div className="flex">
-                  <span className="title-font hidden text-2xl font-medium text-gray-900">
-                    $58.00
-                  </span>
                   <ButtonCTA />
-                  <button className="ml-4 hidden h-10 w-10 items-center justify-center rounded-full border-0 bg-gray-200 p-0 text-gray-500">
-                    <svg
-                      fill="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="hidden h-5 w-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                    </svg>
-                  </button>
                 </div>
               </div>
 
-              <div className="flex w-full flex-col items-center pt-10 lg:w-1/2">
+              <div className="flex w-full flex-col items-center lg:w-1/2">
                 <div className="mb-4 overflow-hidden">
                   <img
                     src={selectedImage}
@@ -171,14 +159,14 @@ const DetailM: React.FC = () => {
                     onClick={() => openModal(selectedImage)}
                   />
                 </div>
-                <div className="flex space-x-4">
+                <div className="flex justify-between space-x-4">
                   {images.map((image, index) => (
                     <img
                       key={index}
                       src={image}
                       alt={`Thumbnail ${index + 1}`}
-                      className={`h-20 w-20 cursor-pointer rounded-lg object-cover ${selectedImage === image ? "border-4 border-blue-500" : ""}`}
-                      onClick={() => openModal(image)}
+                      className={`h-20 w-20 cursor-pointer rounded-lg object-cover transition-transform duration-300 hover:scale-110 ${selectedImage === image ? "border-4 border-blue-500" : ""}`}
+                      onClick={() => setSelectedImage(image)}
                     />
                   ))}
                 </div>
@@ -201,3 +189,4 @@ const DetailM: React.FC = () => {
 };
 
 export default DetailM;
+
